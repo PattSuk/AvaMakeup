765c190a7324ac5611419ee257b91137e4526017
Pattarapond Sukvanachaikul <patt@Pattaraponds-MacBook-Pro.local>
2020-12-15 14:26:32 -0800

    starting the appointment page

--- client/src/pages/Appointments.js
+++ client/src/pages/Appointments.js
@@ -1,7 +1,25 @@
+import axios from 'axios';
+
 function Appointments () {
+    const appointmentsLists = [];
+
+    axios.get(`http://localhost:5000/customers`)
+    .then((response) => {
+        appointmentsLists = response.data;
+    })
+    .catch(error => console.log(error));
+
     return (
         <section className="appointments">
             <h1 className="appointments__title">Appointments</h1>
+            <select>
+                <option value="all">All</option>
+                <option value="notConfirm">Not Confirm</option>
+                <option value="confirmed">Confirmed</option>
+                <option value="denied">Denied</option>
+                <option value="today">Today</option>
+            </select>
+            
         </section>
     )
 }
