56cabe42c926165d4654d4510b3c4b8f677f51cb
Pattarapond Sukvanachaikul <patt@Pattaraponds-MacBook-Pro.local>
2021-01-12 16:09:49 -0800

    updated database

--- server/prismaClient.js
+++ server/prismaClient.js
@@ -4,20 +4,18 @@ const prisma = new PrismaClient({
     log: ['info', 'warn', 'error']
 });
 
-const prisma = process.env === production
+const prisma = process.env !== production
 ? new PrismaClient({
     datasources: {
         my_database: {
-            provider: "mysql",
-            url: env("DATABASE_URL")
+            url: process.env.DATABASE_URL
         }
     }
 })
 : new PrismaClient({
     datasources: {
         production_database: {
-            provider: "mysql",
-            url: process.env.JAWSDB_URL
+            url: process.env.PRODUCTION_DB_URL
         }
     }
 })
